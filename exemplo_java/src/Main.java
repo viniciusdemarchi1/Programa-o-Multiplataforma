54137210add818716cceeed3268b375e8fcb41dc
Vinicius Ferrari <203107476+viniciusdemarchi1@users.noreply.github.com>
2026-05-10 23:18:24 -0300

    10-05 classe funcionario
    aqui fiz a classe funcionario no java e executando no main

--- exemplo_java/src/Main.java
+++ exemplo_java/src/Main.java
@@ -1,14 +1,16 @@
 import classes.Pessoa;
+import classes.Funcionario;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+/*
     for(int i=0 ;i<20; i++){
         Pessoa p1 = new Pessoa();
-        System.out.println("Digite o nome da " + (i+1) + "a pessoa");
+        System.out.println("Digite o nome da " + (i+1) + "ª pessoa");
         p1.setNome(scan.next());
 
         System.out.println("Digite a sua idade");
@@ -18,8 +20,29 @@ public class Main {
         p1.setSalario(scan.nextDouble());
 
         System.out.println(p1.imprimirDadosdePessoas());
+        }
+*/
+        for (int i = 0; i<5; i++){
+            Funcionario f1 = new Funcionario();
+            System.out.println("Digite o nome da " + (i+1) + "ª pessoa");
+            f1.setNome(scan.next());
 
-    }
+            System.out.println("Digite a data (AAAA-MM-DD)");
+            String dataString = scan.next();
+            f1.setData(LocalDate.parse(dataString));
+
+
+            System.out.println("Digite o seu salario: ");
+            f1.setSalario(scan.nextDouble());
+
+            if(f1.getSalario()>5000){
+                System.out.println(f1.bonus());
+            }else{
+                System.out.println(f1.desconto());
+            }
+        }
+
+
 
 
 
--- exemplo_java/src/classes/Funcionario.java
+++ exemplo_java/src/classes/Funcionario.java
@@ -4,4 +4,43 @@ public class Funcionario {
     private String nome;
     private double salario;
     private LocalDate data;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public double bonus(){
+
+            salario = getSalario() * 1.05;
+
+        return salario;
+    }
+
+    public double desconto(){
+        salario = getSalario() * 0.97;
+
+        return salario;
+    }
+
 }
